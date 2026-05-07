ad4d4e48e5d302e4ba60eded962246e1c53a3573
Sumit <25413sumit@gmail.com>
2026-05-07 22:33:25 +0530

    Fix app.js and package.json for Render deployment

--- app.js
+++ app.js
@@ -1,53 +1,58 @@
 const path = require("path");
-
 const express = require("express");
-
 const session = require("express-session");
-
 const mongoose = require("mongoose");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const rootDir = require("./utils/pathUtil");
-
 const storeRouter = require("./routes/storeRouter");
-
 const hostRouter = require("./routes/hostRouter");
-
 const authRouter = require("./routes/authRouter");
-
 const errorsController = require("./controllers/errors");
 
 const app = express();
 
-const DB_PATH =
-  "mongodb+srv://root:root@cluster0.wzflac0.mongodb.net/airbnb?retryWrites=true&w=majority";
+// ENV VARIABLES
+const DB_PATH = process.env.MONGO_URL || "mongodb+srv://root:root@cluster0.wzflac0.mongodb.net/airbnb?retryWrites=true&w=majority";
+const SESSION_SECRET = process.env.SESSION_SECRET || "AirBnb";
+const PORT = process.env.PORT || 3000;
+
+// SESSION STORE (MongoDB)
+const store = new MongoDBStore({
+  uri: DB_PATH,
+  collection: "sessions",
+});
+
+store.on("error", (err) => {
+  console.error("Session store error:", err);
+});
 
 // VIEW ENGINE
 app.set("view engine", "ejs");
-
 app.set("views", "views");
 
 // MIDDLEWARES
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(rootDir, "public")));
 
 // SESSION
 app.use(
   session({
-    secret: "AirBnb",
+    secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-  }),
+    store: store,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+  })
 );
 
 // GLOBAL VARIABLES
 app.use((req, res, next) => {
   res.locals.isLoggedIn = req.session.isLoggedIn || false;
-
   res.locals.role = req.session.role || null;
-
   res.locals.user = req.session.user || null;
-
   next();
 });
 
@@ -57,16 +62,13 @@ app.use(authRouter);
 // HOST PROTECTION + HOST ROUTES
 app.use(
   "/host",
-
   (req, res, next) => {
     if (!req.session.isLoggedIn || req.session.role !== "host") {
       return res.redirect("/login");
     }
-
     next();
   },
-
-  hostRouter,
+  hostRouter
 );
 
 // STORE ROUTES
@@ -76,17 +78,15 @@ app.use(storeRouter);
 app.use(errorsController.pageNotFound);
 
 // SERVER
-const PORT = 3000;
-
 mongoose
   .connect(DB_PATH)
   .then(() => {
     console.log("Connected to MongoDB");
-
     app.listen(PORT, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log(err);
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
   });
